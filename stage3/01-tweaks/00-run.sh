0fc67b5eec9a5d878ede276459097be4865a5149
Michael Kutý <6du1ro.n@gmail.com>
2018-06-15 20:04:11 +0200

    Install hassio
    * Fix bluetooth start with sdp.
    
    * Install MQTT, craftbox-firmware
    
    * Install python 3.6.
    
    * Update hass installation.
    
    * Install hassio.

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -1,17 +1,63 @@
+
 #!/bin/bash -ex
 
 on_chroot << EOF
 groupadd -f -r -g 1001 homeassistant
-useradd -u 1001 -g 1001 -rm homeassistant
+#useradd -u 1001 -g 1001 -rm homeassistant
 EOF
+
+install -v -o 1001 -g 1001 -d ${ROOTFS_DIR}/srv/homeassistant
+mkdir -p files
+
+on_chroot << EOF
+curl -sSL https://get.docker.com | sh
+EOF
+
+# on_chroot << EOF
+# dpkg -i /srv/homeassistant/hassbian-scripts-0.6.deb
+# EOF
+
+# install MQTT
+# on_chroot << EOF
+# wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
+# sudo apt-key add mosquitto-repo.gpg.key -y
+# apt-get update
+# apt-get install mosquitto
+# systemctl enable mosquitto
+# EOF
 
 on_chroot << EOF
 sdptool add SP
-git clone https://github.com/Craftama/rpi3-wifi-conf.git /srv/homeassistant/craftbox-wifi-conf
-chmod +x /srv/homeassistant/craftbox-wifi-conf/run.py
-pip3 install wifi
 
-sed -i -- 's/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd -C/g' /etc/systemd/system/dbus-org.bluez.service
+ssh-keyscan gitlab.com >> ~/.ssh/known_hosts
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+if cd /srv/homeassistant/craftbox-wifi-conf; then git pull; else git clone https://github.com/Craftama/rpi3-wifi-conf.git /srv/homeassistant/craftbox-wifi-conf; fi
+if cd /srv/craftbox-firmware; then git pull; else git clone https://gitlab.com/craftama/craftbox-firmware.git /srv/craftbox-firmware; fi
+
+echo "cs_CZ.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen cs_CZ
+locale-gen en_GB.UTF-8
+
+# enable i2c
+echo "i2c-bcm2708" >> /etc/modules
+echo "i2c-dev" >> /etc/modules
+
+echo "dtparam=i2c1=on" >> /boot/config.txt
+echo "dtparam=i2c_arm=on" >> /boot/config.txt
+
+pip3 install -U pip setuptools
+pip3 install -r /srv/craftbox-firmware/requirements/default.txt
+
+chmod +x /srv/craftbox-firmware/craftbox/cli.py
+chmod +x /srv/homeassistant/craftbox-wifi-conf/run.py
+pip3 install PyBluez wifi
+
+#sed -i -- 's/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd -C/g' /etc/systemd/system/dbus-org.bluez.service
+#sed -i -- 's/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd -C/g' /etc/systemd/system/bluetooth.target.wants/bluetooth.service
+sed -i -- 's/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd -C/g' /lib/systemd/system/bluetooth.service
+
+systemctl daemon-reload
 
 cat >/etc/rc.local <<EOL
 #!/bin/sh -e
@@ -27,10 +73,14 @@ cat >/etc/rc.local <<EOL
 #
 # By default this script does nothing.
 
-# configure bluetooth
-echo -e 'power on\ndiscoverable on\nscan on\t \nquit' | bluetoothctl
 # start wifi configurator
+(sleep 10;PYTHONPATH=/srv/craftbox-firmware/ /srv/craftbox-firmware/craftbox/cli.py)&
 (sleep 10;/srv/homeassistant/craftbox-wifi-conf/run.py)&
+# configure bluetooth
+echo 'power on\ndiscoverable on\nscan on\t \nquit' | bluetoothctl
+
+# install hassio
+curl -sL https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install | bash -s -- -m raspberrypi
 
 # Print the IP address
 _IP=$(hostname -I) || true
@@ -38,22 +88,11 @@ if [ "$_IP" ]; then
   printf "My IP address is %s\n" "$_IP"
 fi
 
+# sdptool add SP
+sdptool add SP
 exit 0
 EOL
 
-EOF
-
-install -v -o 1001 -g 1001 -d ${ROOTFS_DIR}/srv/homeassistant
-mkdir -p files
-wget -O ./files/hassbian-scripts-0.6.deb https://github.com/home-assistant/hassbian-scripts/releases/download/v0.6/hassbian-scripts_0.6.deb
-install -v -m 600 ./files/hassbian-scripts-0.6.deb ${ROOTFS_DIR}/srv/homeassistant/
-
-on_chroot << EOF
-dpkg -i /srv/homeassistant/hassbian-scripts-0.6.deb
-EOF
-
-on_chroot << EOF
-systemctl enable install_homeassistant
 EOF
 
 on_chroot << \EOF
@@ -65,3 +104,6 @@ for GRP in homeassistant; do
 done
 EOF
 
+# on_chroot << EOF
+# /opt/hassbian/suites/install_homeassistant.sh
+# EOF
